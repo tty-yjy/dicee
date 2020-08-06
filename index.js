636c17f5a68aa20ae524f889ebcb7dca1b39da20
tty-yjy <56337230+tty-yjy@users.noreply.github.com>
2020-08-06 22:43:23 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+var randomNum1 = Math.floor(Math.random() * 6) + 1;
+var randomNum2 = Math.floor(Math.random() * 6) + 1;
+
+if (performance.navigation.type === 1) {
+  document.querySelector(".img1").setAttribute("src", "images/dice" + randomNum1 + ".png");
+  document.querySelector(".img2").setAttribute("src", "images/dice" + randomNum2 + ".png");
+
+  if (randomNum1 > randomNum2) {
+    document.querySelector("h1").textContent = "Player 1 Wins!";
+    document.querySelector(".p1").textContent = "🚩Player 1🚩";
+    document.querySelector(".p2").textContent = "Player 2";
+  }
+  else if (randomNum1 < randomNum2) {
+    document.querySelector("h1").textContent = "Player 2 Wins!";
+    document.querySelector(".p1").textContent = "Player 1";
+    document.querySelector(".p2").textContent = "🚩Player 2🚩";
+  }
+  else {
+    document.querySelector("h1").textContent = "It's a Tie!";
+    document.querySelector(".p1").textContent = "Player 1";
+    document.querySelector(".p2").textContent = "Player 2";
+  }
+}
